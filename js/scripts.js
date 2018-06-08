6125561a188f94f63f05d14821ba4cc538912533
Paul <paul.ryan.iii@gmail.com>
2018-06-08 13:16:26 -0700

    Create moving shadow

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-const me = document.querySelector('.me');
+const me = document.querySelector('.header-image');
 const walk = 15; // 15 px
-console.log('me', me);
+
+function shadow(e) {
+  // onj destructuring
+  const { offsetWidth: width, offsetHeight: height } = me;
+  let { offsetX: x, offsetY: y } = e;
+  console.log(x, y);
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2)); // -50 to 50
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  me.style.boxShadow = `${xWalk}px ${yWalk}px 2px #111`;
+}
+
+document.querySelector('#welcome').addEventListener('mousemove', shadow);
